0b2c1fc83353cb989bc52cc834cb591c8ea0e345
Mateusz Moskala <mmoskala07@gmail.com>
2025-04-17 11:25:24 +0200

    Formatting fixed

--- homework/advanced-calculator/main.cpp
+++ homework/advanced-calculator/main.cpp
@@ -5,7 +5,7 @@
 int main() {
     double result = 0;
     std::string command = "-54.31 - -0.69";
-    
+
     ErrorCode rc = process(command, &result);
 
     std::cout << command << " = " << result << std::endl;
